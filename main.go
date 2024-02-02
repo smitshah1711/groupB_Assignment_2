2fa4f05b330a9a4bf60c71b7f250f51ad7138abc
smitshah1711 <61113890+smitshah1711@users.noreply.github.com>
2024-02-01 19:17:35 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Factorial
+	fmt.Print("1.Fictorial -   \n")
+	fmt.Print("author name:Smit Sandeepkumar Shah Student ID:500221322 \n")
+	num := 5
+	factorialResult := factorial(num)
+	fmt.Printf("Factorial of %d: %d\n", num, factorialResult)
+	fmt.Println("-------------------------------------------------------------------")
+}
+
+// author name:Smit Sandeepkumar Shah
+// Student ID:500221322
+func factorial(n int) int {
+	if n == 0 || n == 1 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
 
