abf8679a5d467026b51ebdd99f58e8b1755635dc
hiren <72260974+hirensavani-99@users.noreply.github.com>
2024-02-01 19:34:51 -0500

    greater or less

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
+package main
 
+import "fmt"
+
+func main() {
+
+	fmt.Print("8.check whether the value of variable is greater than or less than 5 \n")
+	fmt.Print("author name:Hirenkumar Savani   Student ID:500226947 \n")
+	// conditional statements to check whether the value of variable is greater than or less than 5
+	var number1 int
+	var number2 int
+
+	fmt.Print("Enter a number: ")
+	fmt.Scan(&number1)
+	fmt.Scan(&number2)
+	greaterOrLess(number1, number2)
+}
+
+func greaterOrLess(number1 int, number2 int) {
+	if number1 > number2 {
+		fmt.Printf(" %d is greater than %d \n", number1, number2)
+	} else {
+		fmt.Printf(" %d is less than %d \n", number1, number2)
+	}
+}
